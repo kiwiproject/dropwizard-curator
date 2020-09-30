d56328ae59732fb0714d4a7dc2e0ce34e61230fc
Scott Leberknight <scottleber@gmail.com>
2020-09-30 09:47:02 -0400

    Add severity to CuratorHealthCheck
    All Curator and/or ZooKeeper problems are considered critical, since
    it means the service will not be able to use Curator, e.g. if Curator
    is not started, is stopped, if there is a ZooKeeper connection problem,
    ZooKeeper is read-only, etc.
    
    Fixes #20

--- src/main/java/org/kiwiproject/curator/health/CuratorHealthCheck.java
+++ src/main/java/org/kiwiproject/curator/health/CuratorHealthCheck.java
@@ -1,5 +1,9 @@
 package org.kiwiproject.curator.health;
 
+import static org.kiwiproject.metrics.health.HealthCheckResults.newHealthyResult;
+import static org.kiwiproject.metrics.health.HealthCheckResults.newUnhealthyResult;
+import static org.kiwiproject.metrics.health.HealthStatus.CRITICAL;
+
 import com.codahale.metrics.health.HealthCheck;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -33,33 +37,33 @@ public class CuratorHealthCheck extends HealthCheck {
     protected Result check() throws Exception {
         var state = client.getState();
         if (state == CuratorFrameworkState.LATENT) {
-            return Result.unhealthy("Curator [ %s ] has not been started - start() has not been called", connectString);
+            return newUnhealthyResult(CRITICAL, "Curator [ %s ] has not been started - start() has not been called", connectString);
         } else if (state == CuratorFrameworkState.STOPPED) {
-            return Result.unhealthy("Curator [ %s ] is stopped", connectString);
+            return newUnhealthyResult(CRITICAL, "Curator [ %s ] is stopped", connectString);
         } else if (state != CuratorFrameworkState.STARTED) {
-            return Result.unhealthy("Curator [ %s ] has unknown state: %s", connectString, state);
+            return newUnhealthyResult(CRITICAL, "Curator [ %s ] has unknown state: %s", connectString, state);
         }
 
         var zookeeperClient = client.getZookeeperClient();
         boolean connected = zookeeperClient.isConnected();
         LOG.trace("CuratorZookeeperClient is connected? {}", connected);
         if (!connected) {
-            return Result.unhealthy("Curator [ %s ] is started but is not connected", connectString);
+            return newUnhealthyResult(CRITICAL, "Curator [ %s ] is started but is not connected", connectString);
         }
 
         var zooKeeperState = zookeeperClient.getZooKeeper().getState();
         LOG.trace("ZK state: {}", zooKeeperState);
         if (zooKeeperState == ZooKeeper.States.CONNECTEDREADONLY) {
-            return Result.unhealthy("ZooKeeperState [ %s ] is connected but is read-only", connectString);
+            return newUnhealthyResult(CRITICAL, "ZooKeeperState [ %s ] is connected but is read-only", connectString);
         }
 
         try {
             List<String> znodes = client.getChildren().forPath("/");
             LOG.trace("Found znodes at root: {}", znodes);
-            return Result.healthy("Curator [ %s ] is healthy", connectString);
+            return newHealthyResult("Curator [ %s ] is healthy", connectString);
         } catch (Exception e) {
             LOG.trace("Error getting root-level znodes", e);
-            return Result.unhealthy("Curator [ %s ] - unable to read znodes at root path '/'", connectString);
+            return newUnhealthyResult(CRITICAL, "Curator [ %s ] - unable to read znodes at root path '/'", connectString);
         }
     }
 }
