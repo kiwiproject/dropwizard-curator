1845d3646c514d129ef57873843a7b123806ae97
Scott Leberknight <174812+sleberknight@users.noreply.github.com>
2023-02-08 07:42:39 -0500

    Handle invalid connect strings in ZooKeeperAvailabilityChecker (#203)
    Add better handling of invalid ZooKeeper connect strings in
    ZooKeeperAvailabilityChecker. IllegalStateException is thrown when:
    - a port is not a number
    - the connection string does not contain host:port pairs
    
    Fixes #202

--- src/test/java/org/kiwiproject/curator/zookeeper/ZooKeeperAvailabilityCheckerTest.java
+++ src/test/java/org/kiwiproject/curator/zookeeper/ZooKeeperAvailabilityCheckerTest.java
@@ -3,6 +3,7 @@ package org.kiwiproject.curator.zookeeper;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
@@ -16,6 +17,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.kiwiproject.curator.config.CuratorConfig;
 import org.kiwiproject.net.SocketChecker;
 import org.kiwiproject.test.junit.jupiter.params.provider.BlankStringArgumentsProvider;
@@ -111,6 +113,38 @@ class ZooKeeperAvailabilityCheckerTest {
         }
 
         @Nested
+        class ShouldThrowIllegalStateException {
+
+            @ParameterizedTest
+            @CsvSource({
+                    "'zk1:foo', foo",
+                    "'zk1:2_182, zk2:2181', 2_182",
+                    "'zk1:2182, zk2:bar, zk3:2181', bar"
+            })
+            void whenGivenInvalidPort(String connectString, String expectedInvalidPort) {
+                System.out.println("connectString = " + connectString);
+                assertThatIllegalStateException()
+                        .isThrownBy(() -> checker.anyZooKeepersAvailable(connectString))
+                        .withCauseExactlyInstanceOf(NumberFormatException.class)
+                        .withMessage("port is not a number: %s", expectedInvalidPort);
+            }
+
+            @ParameterizedTest
+            @CsvSource({
+                    "'zk1, zk2:2181, zk3:2181', zk1",
+                    "'zk1:2181, zk2', zk2",
+                    "'zk1:2181, zk2:', zk2:",
+                    "'zk1:2181, zk2:2181, zk3', zk3"
+            })
+            void whenGivenInvalidHostAndPort(String connectString, String expectedInvalidHostAndPort) {
+                assertThatIllegalStateException()
+                        .isThrownBy(() -> checker.anyZooKeepersAvailable(connectString))
+                        .withNoCause()
+                        .withMessage("host/port pair does not have exactly two elements: %s", expectedInvalidHostAndPort);
+            }
+        }
+
+        @Nested
         class ShouldThrowIllegalArgumentException {
 
             @Test
@@ -128,6 +162,5 @@ class ZooKeeperAvailabilityCheckerTest {
                         .withMessage("ZooKeeper connect string must not be blank");
             }
         }
-
     }
 }
